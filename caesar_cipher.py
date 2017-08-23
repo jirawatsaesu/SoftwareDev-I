1c7b2c656d7a9c033842918846bfb68cc01371cc
Jirawat Yuktawathin <31250964+jirawatsaesu@users.noreply.github.com>
2017-08-23 23:43:11 +0700

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,7 +1,8 @@
 # Caesar Cipher
 #
 # Jirawat Yuktawathin
-# 18/8/2017
+# start  : 18/8/2017
+# finish : 18/8/2017
 
 alphabet_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,\
                 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,'n': 14,\
