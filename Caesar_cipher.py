b6d8eaaea1ff99e7d04a0efe34a0d516240a456f
Jirawat Yuktawathin <31250964+jirawatsaesu@users.noreply.github.com>
2017-09-22 16:51:48 +0700

    Update Caesar_cipher.py

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -2,25 +2,26 @@
 #
 # Jirawat Yuktawathin
 # start  : 16/8/2017
-# finish : 18/8/2017
+# update : 18/8/2017
 
-alphabet_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,\
-                'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,'n': 14,\
-                'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21,\
+alphabet_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,
+                'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
+                'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21,
                 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 0}
-num_alphabet = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g',\
-                8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n',\
-                15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u',\
+num_alphabet = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g',
+                8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n',
+                15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u',
                 22: 'v', 23: 'w', 24: 'x', 25: 'y', 0: 'z'}
 
+
 def encrypt(plain, key):
-    ''' Encrypt words to Caesar cipher with key.'''
+    """ Encrypt words to Caesar cipher with key."""
     global alphabet_num, num_alphabet
     key = key.lower()
     plain = plain.lower()
     cipher = ''
 
-    # Repeat a key if lenght less than a plain text.
+    # Repeat a key if length less than a plain text.
     while len(key) < len(plain):
         key += key
 
@@ -34,14 +35,15 @@ def encrypt(plain, key):
 
     return cipher
 
+
 def decrypt(cipher, key):
-    ''' Decrypt Caesar cipher to words with key.'''
+    """ Decrypt Caesar cipher to words with key."""
     global alphabet_num, num_alphabet
     cipher = cipher.lower()
     key = key.lower()
     plain = ''
 
-    # Repeat a key text if lenght less than a cipher code.
+    # Repeat a key text if length less than a cipher code.
     while len(key) < len(cipher):
         key += key
 
@@ -55,5 +57,5 @@ def decrypt(cipher, key):
 
     return plain
 
-print(encrypt('attack at dawn','diamond'))
-print(decrypt('ecunry ec qpkr','diamond'))
+print(encrypt('attack at dawn', 'diamond'))
+print(decrypt('ecunry ec qpkr', 'diamond'))
