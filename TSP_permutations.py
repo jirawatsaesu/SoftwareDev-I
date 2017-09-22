6900846cefbdc9c4664cf9b41d680f376cf6fc41
Jirawat Yuktawathin <31250964+jirawatsaesu@users.noreply.github.com>
2017-09-22 16:35:50 +0700

    Update TSP_permutations.py

--- TSP_permutations.py
+++ TSP_permutations.py
@@ -3,26 +3,17 @@
 #
 # Jirawat Yuktawathin
 # start  : 9/8/2017
-# finish : 23/8/2017
+# update : 22/9/2017
 #
-# Python version : 2.7
+# Python version : 3.6
 
 import matplotlib.pyplot as plt
 from itertools import permutations
 from random import randrange
-import time
 
-# Random points.
 point = 5
-x = [[], []]
-y = [[], []]
-for i in range(point):
-    x[0].append(i + 1)
-    x[1].append(randrange(0, 100))
-    y[0].append(i + 1)
-    y[1].append(randrange(0, 100))
-#print(x)
-#print(y)
+x = [[i + 1 for i in range(point)], [randrange(100) for i in range(point)]]
+y = [[i + 1 for i in range(point)], [randrange(100) for i in range(point)]]
 
 # Metrix of distances.
 d_metrix = [None] * (point * (point - 1))
@@ -37,25 +28,16 @@ while i < point:
         d_metrix[((i + 1) * (j + 1)) - 1] = distance
         j += 1
     i += 1
-#print(d_metrix)
 
 # All of ways.
 all_way = list(permutations(x[0]))
-print(all_way)
 
 # Create a graph
-plt.ion()
+plt.figure(figsize=(10, 6))
+ax1 = plt.subplot(1, 2, 1)
+ax2 = plt.subplot(1, 2, 2)
 
-fig = plt.figure(figsize=(8, 6))
-ax1 = fig.add_subplot(1, 2, 1)
-line1, = ax1.plot(x[1], y[1], 'r--')
-ax1.plot(x[1], y[1], 'ro')
-
-ax2 = fig.add_subplot(1, 2, 2)
-line2, = ax2.plot(x[1], y[1], 'b--')
-ax2.plot(x[1], y[1], 'bo')
-
-# Find the shortest way and graph update.
+# Find the shortest way then update graph.
 for i in range(len(all_way)):
     d_total = 0
     for j in range(len(all_way[i])):
@@ -70,20 +52,17 @@ for i in range(len(all_way)):
 
     x_coord = [x[1][k - 1] for k in (all_way[i] + (all_way[i][0],))]
     y_coord = [y[1][k - 1] for k in (all_way[i] + (all_way[i][0],))]
-    line1.set_xdata(x_coord)
-    line1.set_ydata(y_coord)
+    ax1.plot(x[1], y[1], 'ro')
+    ax1.plot(x_coord, y_coord, 'r--')
+    ax1.set_title('n = {} Distance = {}'.format(i + 1, d_total))
 
     x_coord_short = [x[1][k - 1] for k in (all_way[short] + (all_way[short][0],))]
     y_coord_short = [y[1][k - 1] for k in all_way[short] + (all_way[short][0],)]
-    line2.set_xdata(x_coord_short)
-    line2.set_ydata(y_coord_short)
-
-    ax1.set_title('n = {} Distance = {}'.format(i + 1, d_total))
+    ax2.plot(x[1], y[1], 'bo')
+    ax2.plot(x_coord_short, y_coord_short, 'b--')
     ax2.set_title('Shortest distance = {}'.format(d_min))
 
-    fig.canvas.draw()
-    time.sleep(0.0001)
-
-time.sleep(5)
-#print(d_min)
-#print(all_way[short])
+    plt.pause(0.01)
+    ax1.cla()
+    ax2.cla()
+plt.show()
